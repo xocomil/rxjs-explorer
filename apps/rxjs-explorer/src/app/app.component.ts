60a98cad3daae26feadda46cc7ca204ec4176350
xocomil <xocomil@gmail.com>
2023-03-27 20:04:22 -0600

    added outer-observable lib and tailwind

--- apps/rxjs-explorer/src/app/app.component.ts
+++ apps/rxjs-explorer/src/app/app.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { FruitsComponent } from '@rxjs-explorer/outer-observables';
 
 @Component({
   selector: 'rxjs-explorer-root',
-  template: `<h1>RxJs</h1>
+  template: `<h1 class="text-blue-700 text-xl">RxJs</h1>
     <p>This works!</p>
-    <p>Here is the title: {{ title }}</p>`,
+    <p>Here is the title: {{ title }}</p>
+    <rxjs-explorer-fruits />`,
   styleUrls: ['./app.component.scss'],
+  imports: [FruitsComponent],
   standalone: true,
 })
 export class AppComponent {
+  @HostBinding() class = 'rounded border-4 bg-slate-100';
+
   title = 'rxjs-explorer';
 }
