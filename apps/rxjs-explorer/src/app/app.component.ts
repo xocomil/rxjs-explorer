948a55903a66308f658c163973ded0fc7f3ac905
xocomil <xocomil@gmail.com>
2023-03-27 18:34:26 -0600

    Initial commit

---
+++ apps/rxjs-explorer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rxjs-explorer-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'rxjs-explorer';
+}
